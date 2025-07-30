f0cc38b61bb957379d6a911bc7ab6635c632ed59
Barys Yarash <barys.yarash@innowise.com>
2025-07-31 00:16:34 +0200

    Added btn handlers

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,84 @@ import './TimeInput.tsx'
 import './TimeControl.tsx'
 import {TimerInput} from "./TimeInput.tsx";
 import {TimeControl} from "./TimeControl.tsx";
+import {useState} from "react";
 
 function App() {
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  
+  const decreaseHours = () => {
+    if (hours > 0) {
+      setHours(hours - 1);
+    }
+  };
+  
+  const increaseHours = () => {
+    setHours(hours + 1);
+  }
+  
+  const decreaseMinutes = () => {
+    if (minutes > 0) {
+      setMinutes(minutes - 1);
+    }
+    else {
+      decreaseHours();
+      setMinutes(59);
+    }
+  }
+  
+  const increaseMinutes = () => {
+    if (minutes < 59) {
+      setMinutes(minutes + 1);
+    }
+    else {
+      increaseHours();
+      setMinutes(0);
+    }
+  }
+  
+  const decreaseSeconds = () => {
+    if (seconds > 0) {
+      setSeconds(seconds - 1);
+    }
+    else {
+      decreaseMinutes();
+      setSeconds(59);
+    }
+  }
+  
+  const increaseSeconds = () => {
+    if (seconds < 59) {
+      setSeconds(seconds + 1);
+    }
+    else {
+      increaseMinutes();
+      setSeconds(0);
+    }
+  }
+  
+  /*useEffect(() => {
+    const interval = setInterval(() => {
+    
+    });
+  })*/
+  
   
   return (
     <>
       <div className={'timer'}>
-        <TimerInput/>
+        <TimerInput
+          hours={hours}
+          minutes={minutes}
+          seconds={seconds}
+          increaseHours={increaseHours}
+          decreaseHours={decreaseHours}
+          increaseMinutes={increaseMinutes}
+          decreaseMinutes={decreaseMinutes}
+          increaseSeconds={increaseSeconds}
+          decreaseSeconds={decreaseSeconds}
+        />
         <TimeControl/>
       </div>
     </>
--- src/TimeInput.tsx
+++ src/TimeInput.tsx
@@ -1,71 +1,24 @@
-import {useState} from "react";
+type Props = {
+	hours: number;
+	minutes: number;
+	seconds: number;
+	increaseHours: () => void;
+	decreaseHours: () => void;
+	increaseMinutes: () => void;
+	decreaseMinutes: () => void;
+	increaseSeconds: () => void;
+	decreaseSeconds: () => void;
+}
 
-export function TimerInput() {
-	const [hours, setHours] = useState(0);
-	const [minutes, setMinutes] = useState(0);
-	const [seconds, setSeconds] = useState(0);
+export function TimerInput({hours, minutes, seconds, increaseHours, decreaseHours, increaseMinutes, decreaseMinutes, increaseSeconds, decreaseSeconds}: Props) {
 	
-	const decreaseHours = () => {
-		if (hours > 0) {
-			setHours(hours - 1);
-		}
-	};
-	
-	const increaseHours = () => {
-		setHours(hours + 1);
-	}
-	
-	const decreaseMinutes = () => {
-		if (minutes > 0) {
-			setMinutes(minutes - 1);
-		}
-		else {
-			decreaseHours();
-			setMinutes(59);
-		}
-	}
-	
-	const increaseMinutes = () => {
-		if (minutes < 59) {
-			setMinutes(minutes + 1);
-		}
-		else {
-			increaseHours();
-			setMinutes(0);
-		}
-	}
-	
-	const decreaseSeconds = () => {
-		if (seconds > 0) {
-			setSeconds(seconds - 1);
-		}
-		else {
-			decreaseMinutes();
-			setSeconds(59);
-		}
-	}
-	
-	const increaseSeconds = () => {
-		if (seconds < 59) {
-			setSeconds(seconds + 1);
-		}
-		else {
-			increaseMinutes();
-			setSeconds(0);
-		}
-	}
 	
 	return (
 		<div className="timer-input">
 			<div className="hours">
 				<label>
 					Hours:
-					<input
-						type="number"
-						min="0"
-						placeholder="HH"
-						value={hours}
-						onChange={e => setHours(Number(e.target.value))} />
+					<input type="number" min="0" placeholder="HH" value={hours} onChange={e => setHours(Number(e.target.value))} />
 					<div className={'controls'}>
 						<button onClick={decreaseHours}>-</button>
 						<button onClick={increaseHours}>+</button>
